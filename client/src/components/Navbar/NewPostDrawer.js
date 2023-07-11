154bbb7f1a05999c05fe07a5cbe6ee8a28ca0fc9
Rif-7 <muhammadrifan974530@gmail.com>
2023-07-11 15:22:11 +0530

    edit new post form to show image preview

--- client/src/components/Navbar/NewPostDrawer.js
+++ client/src/components/Navbar/NewPostDrawer.js
@@ -5,23 +5,41 @@ import {
   useDisclosure,
   Drawer,
   DrawerBody,
+  Image,
   Input,
   DrawerHeader,
   DrawerOverlay,
+  Alert,
+  AlertIcon,
   DrawerContent,
   DrawerCloseButton,
 } from '@chakra-ui/react';
 
 import { AddIcon } from '@chakra-ui/icons';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function NewPostDrawer() {
+  const fileInputRef = useRef(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [selectedImage, setSelectedImage] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
   const [caption, setCaption] = useState('');
 
   const handleImageChange = e => {
-    setSelectedImage(e.target.files[0]);
+    const imageFile = e.target.files[0];
+    setSelectedImage(imageFile);
+
+    if (imageFile) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewImage(reader.result);
+      };
+      reader.readAsDataURL(imageFile);
+    }
+  };
+
+  const handleFileInputButton = () => {
+    fileInputRef.current.click();
   };
 
   const handleCaptionChange = e => {
@@ -31,6 +49,7 @@ export default function NewPostDrawer() {
   const customOnClose = () => {
     setCaption('');
     setSelectedImage(null);
+    setPreviewImage(null);
     onClose();
   };
 
@@ -52,7 +71,7 @@ export default function NewPostDrawer() {
         New Post
       </Button>
       <Drawer
-        placement="left"
+        placement="right"
         size={'md'}
         isOpen={isOpen}
         onClose={customOnClose}
@@ -64,7 +83,7 @@ export default function NewPostDrawer() {
 
           <DrawerBody overflowY={'auto'}>
             <Box as={'form'} mt={10} onSubmit={onSubmit}>
-              <Stack spacing={4}>
+              <Stack spacing={4} alignItems={'center'}>
                 <Input
                   placeholder="Caption"
                   onChange={handleCaptionChange}
@@ -78,12 +97,33 @@ export default function NewPostDrawer() {
                 />
                 <Input
                   type="file"
-                  onChange={handleImageChange}
+                  ref={fileInputRef}
+                  style={{ display: 'none' }}
                   accept="image/png, image/jpeg"
+                  onChange={handleImageChange}
+                />
+                <Button
                   fontFamily={'heading'}
+                  alignSelf={'start'}
                   bg={'gray.200'}
                   color={'gray.800'}
-                />
+                  onClick={handleFileInputButton}
+                >
+                  Choose Image
+                </Button>
+                {selectedImage ? (
+                  <Image
+                    fallback={
+                      <Alert status="error">
+                        <AlertIcon />
+                        There was an error while displaying the image. The image
+                        will not be submitted.
+                      </Alert>
+                    }
+                    src={previewImage}
+                    alt="selected image"
+                  />
+                ) : null}
               </Stack>
               <Button
                 fontFamily={'heading'}
